3f2a575cce4d0b1054b0f0902762148c12325660
kinevinningland <kine.v@me.com>
2026-04-14 20:15:29 +0200

    Describe change

--- src/print.jl
+++ src/print.jl
@@ -2,7 +2,7 @@ using HDF5
 
 function print_results_h5(dataset::String,RT::Result,model::Model,parameters::Parameters)
 
-   file = h5open(string(dataset,"AggrSimResults_noRes.h5"),"w")
+   file = h5open(string(dataset,"AggrSimResults_wRes>=.h5"),"w")
    write(file, "ObjectiveValue", RT.ObjTable)
    
    attrs(file)["NArea"]  = model.NArea
